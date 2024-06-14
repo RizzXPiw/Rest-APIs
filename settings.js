16d84813ce4602ef86bf390a215039ee5856007b
RizzXPiw <161813597+RizzXPiw@users.noreply.github.com>
2024-06-14 21:44:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 global.creator = "Zeltoria";
-global.apikey = ["Elistz", "Mwuhehe", "Rainchy"];
+global.apikey = ["Elistz", "Mwuhehe", "Rainchy","RizzPiw"];
 
 let file = require.resolve(__filename);
 fs.watchFile(file, () => {
