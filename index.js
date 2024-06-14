c00df22187f85a25eb7ad0ec379f83cf566e1e27
RizzXPiw <161813597+RizzXPiw@users.noreply.github.com>
2024-06-14 13:09:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require("express"),
-	cors = require("cors"),
-	secure = require("ssl-express-www");
-const PORT = process.env.PORT || 8080 || 5000 || 3000;
+    cors = require("cors"),
+    secure = require("ssl-express-www");
+const PORT = process.env.PORT || 3000;
+var path = require("path");
 var { color } = require("./lib/color.js");
 
 var apirouter = require("./routes/api");
@@ -14,16 +15,16 @@ app.use(secure);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-	res.sendFile(__path + "/views/home.html");
+    res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 app.get("/docs", (req, res) => {
-	res.sendFile(__path + "/views/index.html");
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.use("/api", apirouter);
 
 app.listen(PORT, () => {
-	console.log(color("Server running on port " + PORT, "green"));
+    console.log(color("Server running on port " + PORT, "green"));
 });
 
 module.exports = app;
