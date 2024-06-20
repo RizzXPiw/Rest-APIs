1d52330546e1cb1d0d546b92cb04985f36c71e08
RizzXPiw <161813597+RizzXPiw@users.noreply.github.com>
2024-06-20 09:19:21 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -627,6 +627,45 @@ router.get("/download/ytmp4", async (req, res, next) => {
   }
 });
 
+router.get("/download/sfile", async (req, res, next) => {
+    var apikey = req.query.apikey;
+    var url = req.query.url;
+
+    if (!apikey) return res.json(loghandler.noapikey);
+    if (!url) {
+        return res.json({
+            status: false,
+            creator: `RizzPiw`,
+            message: "Masukkan URL yang ingin diunduh.",
+        });
+    }
+
+    if (listkey.includes(apikey)) {
+        try {
+        async function sfileDl(url) {
+    let res = await fetch(url);
+    let $ = cheerio.load(await res.text());
+    let filename = $('div.w3-row-padding').find('img').attr('alt');
+    let mimetype = $('div.list').text().split(' - ')[1].split('\n')[0];
+    let filesize = $('#download').text().replace(/Download File/g, '').replace(/\(|\)/g, '').trim();
+    let download = $('#download').attr('href') + '&k=' + Math.floor(Math.random() * (15 - 10 + 1) + 10);
+    return { filename, filesize, mimetype, download };
+}
+            const result = await sfileDl(url);
+            res.json({
+                status: true,
+                creator: `RizzPiw`,
+                result: result,
+            });
+        } catch (e) {
+            console.log(e);
+            res.json({ status: false, message: e.message });
+        }
+    } else {
+        res.json(loghandler.apikey);
+    }
+});
+
 // news
 router.get("/news/cnn", async (req, res, next) => {
 	var apikey = req.query.apikey;
